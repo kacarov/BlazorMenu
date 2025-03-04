a33a6cf2e900708465efe66f66b8b8f8c8c9b6d8
Martin.Katsarov@entaingroup.com <Martin.Katsarov@entaingroup.com>
2025-03-05 01:42:56 +0200

    Initial commit

---
+++ BlazorMenuComponent/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorMenuComponent.Models;
+
+public class Item
+{
+    public string Name { get; set; }
+
+    public List<Item> SubItems { get; set; }
+
+    public bool IsExpanded { get; set; }
+}
